5959aa98134ef6baa8f6c0d6ce852a11521e918e
luisa-ganasevici <lganasevici@gmail.com>
2025-04-03 22:15:06 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,30 @@
+numero1 = int (input ("digite o numero 1: "))
+numero2 = int (input ("digite o numero 2: "))
+
+soma = numero1 + numero2 
+print (soma)
+sub = numero1 - numero2 
+print (sub)
+
+div = numero1 / numero2 
+resto = numero1 % numero2
+
+print (div, resto)
+
+
+
+pot = numero1 ** numero2
+print (pot)
+
+print ("********************************")
+
+pi = float (3.14)
+area = (pi **2)
+print (area)
+
+
+
+raio = int (input ("digite o raio: "))
+
+cperi = 2 * pi * raio
+print (cperi)
---
+++ aula3.py
@@ -0,0 +1,11 @@
+preco_prod = int(input("digite o preço: "))
+desconto  = int(input("digite o desconto: "))
+desconto_total = preco_prod * desconto / 100
+print (desconto_total)
+
+print ("*****************************************")
+
+espaco = float(input("digite o espaço: "))
+tempo = float(input("digite o tempo: "))
+vm = espaco / tempo
+print (vm)
